abf9a80f8c3066b3d180ceb11ccb40c8e95433d9
djgupta2013 <djgupta2013@gmail.com>
2018-03-09 18:17:23 +0530

    google add

--- ThreadPractise/src/google/Google.java
+++ ThreadPractise/src/google/Google.java
@@ -4,7 +4,8 @@ public class Google {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello google");
+		System.out.println("hello google");
+
 	}
 
 }
