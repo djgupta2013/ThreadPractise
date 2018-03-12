d0b330ae571c165845c9e5332dab2c1fd7af8c4c
djgupta2013 <djgupta2013@gmail.com>
2018-03-12 15:25:44 +0530

    hi

--- ThreadPractise/src/dj/Dhananjay.java
+++ ThreadPractise/src/dj/Dhananjay.java
@@ -5,20 +5,12 @@ public class Dhananjay {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Dhananjay");
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 		System.out.println("12345");
->>>>>>> refs/heads/newBranch
-=======
+
 		System.out.println("12345");
-=======
-<<<<<<< HEAD
-=======
+
 		System.out.println("12345");
->>>>>>> refs/heads/newBranch
->>>>>>> refs/heads/master
->>>>>>> refs/remotes/origin/newBranch
 
 	}
 
