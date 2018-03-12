8be13bdd5741362053da8a33a3e7c4fb2e94b336
djgupta2013 <djgupta2013@gmail.com>
2018-03-12 15:34:11 +0530

    hi

--- ThreadPractise/src/abc/ABC.java
+++ ThreadPractise/src/abc/ABC.java
@@ -2,7 +2,12 @@ package abc;
 
 public class ABC {
 
+<<<<<<< HEAD
 	public static void main(String[] args) {
+=======
+	public static void main(String[] args) {
+
+>>>>>>> refs/remotes/origin/newBranch
 	}
 
 }
